16f87a56aa9ed9d667147e3f26da3f8b4c5e27db
AbdullahAlMaksud <maksud.workspace@gmail.com>
2024-02-19 22:59:22 +0600

    Fix Some funtionality Problem

--- js/script.js
+++ js/script.js
@@ -9,7 +9,6 @@ for (const seat of seats) {
     seat.addEventListener('click', function (e) {
         const seatNumber = e.target.innerText;
         if (!selectedSeatArray.includes(seatNumber)) {
-
             if (selectedSeatArray.length >= 4) {
                 alert('At a time you can select Maximum 4 Seat.');
                 return;
@@ -32,7 +31,6 @@ for (const seat of seats) {
             const selectedPrice = parseInt(document.getElementById('selected-price').innerText);
             selectedTotalPrice = selectedTotalPrice + selectedPrice;
         }
-
         else if (selectedSeatArray.includes(seatNumber)) {
             if (selectedSeatArray.length >= 4) {
                 alert('At a time you can select Maximum 4 Seat.');
@@ -44,7 +42,6 @@ for (const seat of seats) {
             setInnerText('seat-count-indicator', seatCount);
             seatRemain = 40 - selectedSeatArray.length;
             setInnerText('remaining-seat', seatRemain);
-
             const ticketDisplayElement = document.createElement('tr');
             const ticketDisplay = document.getElementById('price-display');
             ticketDisplay.removeChild(ticketDisplayElement);
@@ -54,16 +51,15 @@ for (const seat of seats) {
         else {
             e.target.classList.toggle('bg-themecolor');
         }
-        console.log(selectedSeatArray);
-
 
         setInnerText('total-price', selectedTotalPrice);
         setInnerText('grand-price', selectedTotalPrice);
-        const coupleCoupon = textReadyToCompare('couple-coupon');
-        const newCoupon = textReadyToCompare('new-coupon');
+
+        const newCoupon = document.getElementById('new-coupon').innerText;
+        const coupleCoupon = document.getElementById('couple-coupon').innerText;
         const couponInput = document.getElementById('coupon-input');
         couponInput.addEventListener('input', function () {
-            const inputText = inputReadyToCompare(couponInput);
+            const inputText = couponInput.value;
             if (inputText == coupleCoupon && parseInt(seatCount) >= 4) {
                 const applyButton = document.getElementById('apply-button');
                 applyButton.attributes.removeNamedItem('disabled');
@@ -89,36 +85,23 @@ for (const seat of seats) {
             }
         })
 
+        if (selectedSeatArray.length > 0) {
+            const phoneNumberInputField = document.getElementById('phone-number');
+            phoneNumberInputField.addEventListener('input', function () {
+                let phoneNumber = parseInt(phoneNumberInputField.value);
+                if (typeof phoneNumber === 'number') {
+                    document.getElementById('next-button').attributes.removeNamedItem('disabled');
+                }
+                else {
+                    document.getElementById('next-button').disabled = true
+                }
+            })
+        }
     })
 }
-
-const phoneNumberInputField = document.getElementById('phone-number');
-phoneNumberInputField.addEventListener('input', function () {
-    let phoneNumber = parseInt(phoneNumberInputField.value);
-    if (typeof phoneNumber === 'number' && phoneNumber.toString().length >= 10) {
-        document.getElementById('next-button').attributes.removeNamedItem('disabled');
-    }
-    else {
-        document.getElementById('next-button').disabled = true
-    }
-})
-
 
 //Utility Function
 function setInnerText(idName, value) {
     const newElement = document.getElementById(idName);
     newElement.innerText = value;
-}
-
-function textReadyToCompare(elementID) {
-    const elementText = document.getElementById(elementID).innerText.toLocaleLowerCase();
-    const elementTextSplit = elementText.split(' ');
-    const textReady = elementTextSplit.join('');
-    return textReady;
-}
-function inputReadyToCompare(elementID) {
-    const elementText = (elementID).value.toLocaleLowerCase();
-    const elementTextSplit = elementText.split(' ');
-    const textReady = elementTextSplit.join('');
-    return textReady;
 }
